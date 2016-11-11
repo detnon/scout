dd3ce89f6abef155dd7d6bf667b1530343688222
sam.detnon <sam.detnon@headlondon.com>
2016-11-11 18:01:39 +0000

    `PortCheck` will check for a given port and return a message to state wether it is avaliable or not

--- src/main/java/PortCheck.java
+++ src/main/java/PortCheck.java
@@ -1,5 +1,32 @@
+import java.io.IOException;
+import java.net.Socket;
+
 /**
  * Created by sam on 11/11/2016.
  */
 public class PortCheck {
+
+    public static boolean isAvailable(int port) {
+        System.out.println("--------------Testing port " + port);
+        Socket s = null;
+        try {
+            s = new Socket("localhost", port);
+
+            // If the code makes it this far without an exception it means
+            // something is using the port and has responded.
+            System.out.println("--------------Port " + port + " is not available");
+            return false;
+        } catch (IOException e) {
+            System.out.println("--------------Port " + port + " is available");
+            return true;
+        } finally {
+            if( s != null){
+                try {
+                    s.close();
+                } catch (IOException e) {
+                    throw new RuntimeException("Something went wrong!." , e);
+                }
+            }
+        }
+    }
 }
--- src/main/java/Scout.java
+++ src/main/java/Scout.java
@@ -4,5 +4,9 @@
 class Scout {
     public static void main(String args[]){
         System.out.println(" ");
+
+        PortCheck.isAvailable(22);
+        PortCheck.isAvailable(23);
+        PortCheck.isAvailable(24);
     }
 }
